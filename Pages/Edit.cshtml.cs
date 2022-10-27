f8eebf76d7978ccdf2726fb8012c443da9dcc119
dhlopez <dhelaman@hotmail.com>
2022-10-26 23:12:46 -0400

    Add project files.

---
+++ Pages/Edit.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookStore.Pages
+{
+    public class EditModel : PageModel
+    {
+        private readonly IBookStoreData books;
+
+        public Book book { get; set; }
+
+        public EditModel(IBookStoreData books)
+        {
+            this.books = books;
+        }
+
+        public IActionResult OnGet(int? id)
+        {
+            if (id.HasValue)
+                book = books.GetById(id.Value);
+
+            if (book == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+
+
+            return Page();
+
+        }
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookStore.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace BookStore.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly IBookStoreData booksData;
+        private readonly ILogger<IndexModel> logger;
+
+        public IEnumerable<Book> Books;
+
+        public IndexModel(IBookStoreData booksData, ILogger<IndexModel> logger)
+        {
+            this.booksData = booksData;
+            this.logger = logger;
+        }
+
+        public void OnGet()
+        {
+            Books = booksData.GetAll();
+        }
+    }
+}
